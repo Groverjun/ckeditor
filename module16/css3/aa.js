050930d23a57623f07ae006619f556b66351cc1f
Groverjun <1250901900@qq.com>
2018-11-09 17:33:24 +0800

    add

---
+++ module16/css3/aa.js
@@ -0,0 +1,61 @@
+
+
+    
+
+
+
+
+
+
+    
+
+
+    "van-transition": "miniprogram_npm/vant-weapp/transition/index",
+
+    "van-checkbox": "miniprogram_npm/vant-weapp/checkbox/index",
+
+    "van-checkbox-group": "miniprogram_npm/vant-weapp/checkbox-group/index",
+
+
+    "van-field": "miniprogram_npm/vant-weapp/field/index",
+
+    "van-radio": "miniprogram_npm/vant-weapp/radio/index",
+
+    "van-radio-group": "miniprogram_npm/vant-weapp/radio-group/index",
+
+
+    "van-search": "miniprogram_npm/vant-weapp/search/index",
+    "van-slider": "miniprogram_npm/vant-weapp/slider/index",
+
+
+    "van-stepper": "miniprogram_npm/vant-weapp/stepper/index",
+
+    "van-switch": "miniprogram_npm/vant-weapp/switch/index",
+
+    "van-action-sheet": "miniprogram_npm/vant-weapp/action-sheet/index",
+
+    "van-dialog": "miniprogram_npm/vant-weapp/dialog/index",
+
+    "van-toast": "miniprogram_npm/vant-weapp/toast/index",
+
+    "van-notify": "miniprogram_npm/vant-weapp/notify/index",
+
+    "van-swipe-cell": "miniprogram_npm/vant-weapp/swipe-cell/index",
+
+    "van-switch-cell": "miniprogram_npm/vant-weapp/switch-cell/index",
+
+    "van-tree-select": "miniprogram_npm/vant-weapp/tree-select/index",
+
+    "van-area": "miniprogram_npm/vant-weapp/area/index",
+
+    "van-card": "miniprogram_npm/vant-weapp/card/index",
+
+
+    "van-submit-bar": "miniprogram_npm/vant-weapp/submit-bar/index",
+
+    "van-goods-action": "miniprogram_npm/vant-weapp/goods-action/index",
+
+    "van-goods-action-icon": "miniprogram_npm/vant-weapp/goods-action-icon/index",
+
+
+    "van-goods-action-button": "miniprogram_npm/vant-weapp/goods-action-button/index"
